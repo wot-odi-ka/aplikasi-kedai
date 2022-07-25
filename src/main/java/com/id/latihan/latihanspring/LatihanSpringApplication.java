2bbb4c1c9a9b47779208481a50d4be35538ee7bd
odisaputra <saputraodi393@gmail.com>
2022-07-25 13:40:58 +0700

    commit cros origin

--- src/main/java/com/id/latihan/latihanspring/LatihanSpringApplication.java
+++ src/main/java/com/id/latihan/latihanspring/LatihanSpringApplication.java
@@ -19,7 +19,7 @@ public class LatihanSpringApplication {
        return new WebMvcConfigurer() {
            @Override
            public void addCorsMappings(CorsRegistry registry) {
-               registry.addMapping("/*").allowedOrigins("*");
+               registry.addMapping("/**").allowedOrigins("*");
            }
 
        };
